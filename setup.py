99f9adb5db73e254fb049866f2499e7c248f0b78
WingedAshur6 <ddfe530tx@gmail.com>
2024-10-11 07:56:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,11 @@ import numpy
 
 # Version number
 version = '1.0'
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(name = 'AAGP',
       version = version,
-      author = read('AUTHORS.txt'),
-      description = ("Adjacency-Adaptive Gaussian Process"),
-      license = "BSD 3-clause",
-      keywords = "gaussian process",
-      url = "",
-      long_description=read('README.md'),
-      packages = ['AAGP'],
+      packages = find_packages(),
       install_requires=[
           'matplotlib>=3.5.3','pandas>=1.3.5','scipy>=1.7.3','xgboost>=1.6.2',
-          'scikit-learn>=1.0.2','joblib==1.2.0','numpy>=1.21.6','tqdm==4.65.0','seaborn==0.12.2',
-          # 'deepgp@https://github.com/SheffieldML/PyDeepGP.git#egg=deepgp',
-      ],
+          'scikit-learn>=1.0.2','joblib==1.2.0','numpy>=1.21.6','tqdm==4.65.0','seaborn==0.12.2'
+      ]
       )
