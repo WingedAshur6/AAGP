8b6eec5c8c560ce76865a72c766285fca35eb5fd
WingedAshur6 <ddfe530tx@gmail.com>
2024-10-11 08:16:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(name = 'AAGP',
       install_requires=[
           'matplotlib>=3.5.3','pandas>=1.3.5','scipy>=1.7.3','xgboost>=1.6.2',
           'scikit-learn>=1.0.2','joblib==1.2.0','numpy>=1.21.6','tqdm==4.65.0','seaborn==0.12.2',
+            'GPy==1.10.0'
       ]
       )
