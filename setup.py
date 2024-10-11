33ac0aa53983f73dbdc4f2cfd59aff0ddd10a4ed
WingedAshur6 <ddfe530tx@gmail.com>
2024-10-11 08:31:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,14 @@ setup(name = 'AAGP',
           'matplotlib>=3.5.3','pandas>=1.3.5','scipy>=1.7.3','xgboost>=1.6.2',
           'scikit-learn>=1.0.2','joblib==1.2.0','numpy>=1.21.6','tqdm==4.65.0','seaborn==0.12.2',
             'GPy==1.10.0'
-      ]
+      ],
+      classifiers=[
+                   # 'Natural Language :: English',
+                   # 'Operating System :: MacOS :: MacOS X',
+                   # 'Operating System :: Microsoft :: Windows',
+                   # 'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   ]
       )
