f9a2f59e205b139d4d257005a5e36015c6532d59
WingedAshur6 <ddfe530tx@gmail.com>
2024-10-11 08:13:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name = 'AAGP',
       install_requires=[
           'matplotlib>=3.5.3','pandas>=1.3.5','scipy>=1.7.3','xgboost>=1.6.2',
           'scikit-learn>=1.0.2','joblib==1.2.0','numpy>=1.21.6','tqdm==4.65.0','seaborn==0.12.2',
-      'git+https://github.com/SheffieldML/PyDeepGP.git']
+      'PyDeepGP@https://github.com/SheffieldML/PyDeepGP.git']
       )
